e78609d068b38b9fb1963e2e5ddf4fa046c0f333
imaymine <75173130+imaymine@users.noreply.github.com>
2020-12-19 21:39:15 +0200

    Update event.c

--- event.c
+++ event.c
@@ -1,24 +1,28 @@
 #ifndef EVENT_H_
 #define EVENT_H_
+
 #include "date.h"
 #include <string.h>
-#include <stdio.h>
-#include <malloc.h>
 #include <stdlib.h>
 #include "priority_queue.h"
 #include "member.h"
 
 typedef struct Event_t {
-    char* EventName;
+    char *EventName;
     Date EventDate;
     int event_id;
     PriorityQueue Members;
 } *Event;
 
-Event copyEvent(Event event)
-{
+Event copyEvent(Event event) {
     Event event_new = malloc(sizeof(*event));
+    if (event_new == NULL) {
+        return NULL;
+    }
     event_new->EventName = malloc(sizeof(char) * (strlen(event->EventName) + 1));
+    if (event_new->EventName == NULL) {
+        return NULL;
+    }
     strcpy(event_new->EventName, event->EventName);
     event_new->EventDate = dateCopy(event->EventDate);
     event_new->event_id = event->event_id;
@@ -26,83 +30,80 @@ Event copyEvent(Event event)
     return event_new;
 }
 
-void destroyEvent(Event event)
-{
+void destroyEvent(Event event) {
     dateDestroy(event->EventDate);
     pqDestroy(event->Members);
     free(event->EventName);
     free(event);
 }
 
-PQElement copyEventPQElement(PQElement event)
-{
+PQElement copyEventPQElement(PQElement event) {
     return copyEvent(event);
 }
 
-PQElementPriority copyEventPriority(PQElementPriority priority)
-{
+PQElementPriority copyEventPriority(PQElementPriority priority) {
     PQElementPriority new_priority = malloc(sizeof(new_priority));
+    if (new_priority == NULL) {
+        return NULL;
+    }
     memcpy(new_priority, priority, sizeof(PQElementPriority));
     return new_priority;
 }
 
-void freePQEvent(PQElement event)
-{
+void freePQEvent(PQElement event) {
     destroyEvent(event);
 }
 
-void freePQEventPriority(PQElementPriority priority)
-{
+void freePQEventPriority(PQElementPriority priority) {
     free(priority);
 }
 
-bool equalPQEvents(PQElement element1, PQElement element2)
-{
-    Event event1 = (Event)element1, event2 = (Event)element2;
+bool equalPQEvents(PQElement element1, PQElement element2) {
+    Event event1 = (Event) element1, event2 = (Event) element2;
     return event1->event_id == event2->event_id;
 }
 
-int comparePQEventPriorities(PQElementPriority priority1, PQElementPriority priority2)
-{
-    int* event_priority1 = priority1;
-    int* event_priority2 = priority2;
+int comparePQEventPriorities(PQElementPriority priority1, PQElementPriority priority2) {
+    int *event_priority1 = priority1;
+    int *event_priority2 = priority2;
     return *event_priority2 - *event_priority1;
 }
 
-Event createEvent(char* name, Date date, int event_id)
-{
+Event createEvent(char *name, Date date, int event_id) {
     Event event = malloc(sizeof(*event));
+    if (event == NULL) {
+        return NULL;
+    }
     event->EventName = malloc(sizeof(char) * (strlen(name) + 1));
+    if (event->EventName == NULL) {
+        return NULL;
+    }
     strcpy(event->EventName, name);
     event->EventDate = dateCopy(date);
     event->event_id = event_id;
-    event->Members = pqCreate(copyMemberPQElement, freeMemberElement, equalMemberElements, copyMemberPriority, freeMemberElementPriority, compareMemberElementPriorities);
+    event->Members = pqCreate(copyMemberPQElement, freeMemberElement, equalMemberElements, copyMemberPriority,
+                              freeMemberElementPriority, compareMemberElementPriorities);
     return event;
 }
 
-char* eventGetName(Event event)
-{
+char *eventGetName(Event event) {
     return event->EventName;
 }
 
-Date eventGetDate(Event event)
-{
+Date eventGetDate(Event event) {
     return event->EventDate;
 }
 
-int eventGetId(Event event)
-{
+int eventGetId(Event event) {
     return event->event_id;
 }
 
-PriorityQueue eventGetMembers(Event event)
-{
+PriorityQueue eventGetMembers(Event event) {
     return event->Members;
 }
 
-void eventSetDate(Event event, Date date)
-{
-    event->EventDate=date;
+void eventSetDate(Event event, Date date) {
+    event->EventDate = date;
 }
 
 
